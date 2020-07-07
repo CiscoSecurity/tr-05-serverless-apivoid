c93fbb958b7068ebce14c835ec587ec0e869a34d
AnastasiiaRozlyvan <48979187+AnastasiiaRozlyvan@users.noreply.github.com>
2020-07-07 19:01:12 +0300

    [CCTRI-1347] Query for Sightings (#3)
    * [CCTRI-1347] Query for Sightings
    
    * Deleted blank line at the end of file
    
    * Clean Up
    
    * Add end_time
    
    * Refactoring
    
    * Move CTIM_DEFAULTS to config.py

--- config.py
+++ config.py
@@ -12,3 +12,15 @@ class Config:
                   '<tr-integrations-support@cisco.com>')
 
     API_URL = 'https://endpoint.apivoid.com/{endpoint}/v1/pay-as-you-go/'
+
+    CTR_DEFAULT_ENTITIES_LIMIT = 100
+
+    try:
+        CTR_ENTITIES_LIMIT = int(os.environ['CTR_ENTITIES_LIMIT'])
+        assert CTR_ENTITIES_LIMIT > 0
+    except (KeyError, ValueError, AssertionError):
+        CTR_ENTITIES_LIMIT = CTR_DEFAULT_ENTITIES_LIMIT
+
+    CTIM_DEFAULTS = {
+        'schema_version': '1.0.17',
+    }
